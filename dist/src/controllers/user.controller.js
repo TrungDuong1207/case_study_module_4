78ab7c482c4ce24879c7f5f621b85d6505efe143
tuanba3257 <tuanba.ous@mcredit.com.vn>
2022-12-22 23:40:20 +0700

    about anh mavbar

--- dist/src/controllers/user.controller.js
+++ dist/src/controllers/user.controller.js
@@ -5,6 +5,12 @@ class UserController {
     static async showUserPage(req, res) {
         res.render("user/homeUser");
     }
+    static showAboutPage(req, res) {
+        res.render('user/about');
+    }
+    static contact(req, res) {
+        res.render('user/contact');
+    }
 }
 exports.UserController = UserController;
 //# sourceMappingURL=user.controller.js.map
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -6,4 +6,12 @@ export class UserController {
     static async showUserPage(req, res) {
         res.render("user/homeUser");
     }
+
+    static showAboutPage(req, res) {
+        res.render('user/about')
+    }
+
+    static contact (req, res) {
+        res.render('user/contact')
+    }
 }
